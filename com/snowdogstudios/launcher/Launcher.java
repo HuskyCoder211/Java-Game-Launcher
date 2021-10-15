f260e1590a7a083815541d8ae01ba8c629873567
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-15 15:22:53 -0400

    Update Launcher.java

--- com/snowdogstudios/launcher/Launcher.java
+++ com/snowdogstudios/launcher/Launcher.java
@@ -4,6 +4,7 @@ import com.snowdogstudios.util.Logger;
 import com.snowdogstudios.crashes.CrashReporter;
 import com.snowdogstudios.launcher.window.Window;
 import com.snowdogstudios.launcher.updater.Updater;
+import installations.ProcessStarter;
 
 public class Launcher {
 	public static String status = "ready";
@@ -20,7 +21,8 @@ public class Launcher {
 	}
 	public static void launch() {
 		try {
-			Precess game = Runtime.getRuntime().exec("java -jar build.jar");
+			ProcessStarter.start("build");
+			Process game = ProcessStarter.getCurrentRunningProcess();
 		} catch(Exception e) {
 			launchLogger.log("Launch Failled: "+e.toString());
 			reporter.report("Launch Failled: "+e.toString());
