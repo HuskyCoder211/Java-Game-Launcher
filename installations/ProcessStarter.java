65869dd58977881aa0a2243a4deb0c320c6d616f
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-15 15:26:28 -0400

    Update ProcessStarter.java

--- installations/ProcessStarter.java
+++ installations/ProcessStarter.java
@@ -4,14 +4,22 @@ import com.snowdogstudios.util.Logger;
 import com.snowdogstudios.crashes.CrashReporter;
 
 public class ProcessStarter {
+  public static Process current;
+  CrashReporter reporter = new CrashReporter();
+  Logger processLogger = new Logger();
+  
   public static void start(String packagePath) {
     try {
       Process gameProcess = Runtime.getRuntime().exec("java -cp "+PackagePath);
+      Process current = gameProcess;
     } catch(Exception e) {
-      CrashReporter reporter = new CrashReporter();
-      Logger processLogger = new Logger();
       reporter.report("Failled to start game process: "+e.toString());
       processLogger.log("Failed to start game process: "+e.toString());
     }
+  }
+  
+  public static Process getCurrentRunningProcess() {
+    return current;
+    processLogger.log("Returned current running process");
   }
 }
