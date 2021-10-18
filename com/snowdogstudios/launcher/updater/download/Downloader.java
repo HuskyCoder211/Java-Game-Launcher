e7600298fab10cbfd815158dd40d6dda847fcd2d
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-18 08:48:00 -0400

    Update Downloader.java

--- com/snowdogstudios/launcher/updater/download/Downloader.java
+++ com/snowdogstudios/launcher/updater/download/Downloader.java
@@ -1,7 +1,26 @@
 package com.snowdogstudios.launcher.updater.download;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import com.snowdogstudios.util.Logger;
+
 public class Downloader {
-  public static void download(String path, String dest) {
-    
+  public static Logger logger = new Logger();
+  public static void download(String path, String name) {
+    try {
+      InputStream in = new URL(path).openStream();
+      Files.copy(in, Paths.get(name));
+    } catch(IOException e) {
+      logger.log(e.toString());
+    }
   }
 }
