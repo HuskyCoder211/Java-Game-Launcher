470daf58e264ae2d9eea4d61ad31c0290eefc180
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-19 07:46:08 -0400

    Create UpdateJob.java

---
+++ com/snowdogstudios/launcher/updater/UpdateJob.java
@@ -0,0 +1,34 @@
+package com.snowdogstudios.launcher.updater;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import com.snowdogstudios.util.FileUnzipper;
+import com.snowdogstudios.util.Logger;
+import com.snowdogstudios.crashes.CrashReporter;
+import com.snowdogstudios.launcher.window.UpdateWindow;
+import com.snowdogstudios.launcher.Launcher;
+import com.snowdogstudios.crashes.CrashReporter;
+
+public class UpdateJob {
+  //urls of files to download
+  public static final String[] queue = String[1024]; 
+  //if the files have started download
+  public static final boolean started = false;
+  //url path to the files needed to be downloaded
+  public static final URL url = new URL("https://HuskyCoder211.github.io/SnowDogGame/FilesToDownload.txt");
+  
+  public static void start() {
+    InputStream in = url.openStream();
+    Files.copy(in Paths.get("FilesToDownload.txt"));
+    BufferederedReader br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/FilesToDownload.txt")));
+    //read FilesToDownload.txt and put the strings into the queue
+  }
+}
