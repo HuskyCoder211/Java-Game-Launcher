a7ac85dae9cbc9589091e0ae8418ee4fe0a12515
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-18 20:56:46 -0400

    Add files via upload

---
+++ com/snowdogstudios/launcher/Launcher.java
@@ -0,0 +1,32 @@
+package com.snowdogstudios.launcher;
+
+import java.io.OutputStream;
+import com.snowdogstudios.util.Logger;
+import com.snowdogstudios.crashes.CrashReporter;
+import com.snowdogstudios.launcher.window.Window;
+import com.snowdogstudios.launcher.updater.Updater;
+import installations.ProcessStarter;
+
+public class Launcher {
+	public static String status = "ready";
+	public static CrashReporter reporter = new CrashReporter();
+	public static Logger launchLogger = new Logger();
+	public static void main(String[] args) {
+		try {
+			Updater.checkForUpdate();
+		} catch(Exception e) {
+			reporter.report(e.toString());
+		}
+		new Window();
+	}
+	public static void launch() {
+		try {
+			ProcessStarter.start("build.jar");
+			OutputStream gameOutput = ProcessStarter.getCurrentProcessOutputstream();
+			System.out.println(gameOutput);
+		} catch(Exception e) {
+			launchLogger.log("Launch Failled: "+e.toString());
+			reporter.report("Launch Failled: "+e.toString());
+		}
+	}
+}
---
+++ com/snowdogstudios/util/Logger.java
@@ -0,0 +1,11 @@
+package com.snowdogstudios.util;
+
+public class Logger {
+  public Logger() {
+    
+  }
+  
+  public static void log(String data) {
+    System.out.println(data);
+  }
+}
