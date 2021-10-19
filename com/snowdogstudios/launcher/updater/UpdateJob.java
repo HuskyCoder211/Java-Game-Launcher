fc541175db142710b7b7e93a8a5d67eb2307dfcb
HuskyCoder211 <84343380+HuskyCoder211@users.noreply.github.com>
2021-10-19 11:50:40 -0400

    Update UpdateJob.java

--- com/snowdogstudios/launcher/updater/UpdateJob.java
+++ com/snowdogstudios/launcher/updater/UpdateJob.java
@@ -19,12 +19,14 @@ import com.snowdogstudios.crashes.CrashReporter;
 import com.snowdogstudios.download.Downloader;
 
 public class UpdateJob {
-  //urls of files to download
+  //urls of files to download and their save paths
   public static final String[] queue = String[1024]; 
+  public static final String[] pathsQueue = String[1024]; 
   //if the files have started download
   public static final boolean started = false;
   //url path to the files needed to be downloaded
   public static final URL url = new URL("https://HuskyCoder211.github.io/SnowDogGame/FilesToDownload.txt");
+  public static final URL pathsUrl = new URL("https://HuskyCoder211.github.io/SnowDogGame/FilesToDownloadPaths.txt");
   
   public static Logger logger = new Logger();
   
@@ -32,23 +34,39 @@ public class UpdateJob {
     InputStream in = url.openStream();
     Files.copy(in Paths.get("FilesToDownload.txt"));
     BufferederedReader br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/FilesToDownload.txt")));
+    
     //read FilesToDownload.txt and put the strings into the queue
-    int current = 0;
-    while (current<1024) {
-      queue[current] = br.readLine();
-      current++;
+    String current = "";
+    int num = 0;
+    while (current!=null) {
+      current = br.readLine();
+      queue[num] = current;
+      num++;
+    }
+    
+    InputStream in = pathsUrl.openStream();
+    Files.copy(in Paths.get("FilesToDownloadPaths.txt"));
+    BufferedReader br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/FileToDownloadPaths.txt")));
+    
+    //read FilesToDownloadPaths.txt and put the strings in the path queue
+    current = "";
+    num = 0;
+    while (current!=null) {
+      current = br.readLine();
+      pathsQueue[num] = current;
+      num++;
     }
     
     startFileDownloads();
   }
   
   public static void startFileDownloads() {
-    int current = 0;
     started = true;
-    while (current<1024) {
-      logger.log("Downloading "+queue[current]);
-      Downloader.download(new URL(queue[current]), System.getProperty("user.dir")+"/com/snowdogstudios/game/", ".class");
-      current++;
+    int num = 0;
+    while (num<queue.length) {
+      logger.log("Downloading "+queue[num]);
+      Downloader.download(new URL(queue[num]), System.getProperty("user.dir")+pathsQueue[num], ".class");
+      num++;
     }
     endDownloads();
   }
